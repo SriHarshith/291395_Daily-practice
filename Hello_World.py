145607903f2c32c6c70e25c8a8d0d6955802c42e
SriHarshith <sriharshith2@gmail.com>
2021-04-20 23:40:21 +0530

    Initial Commit

---
+++ Hello_World.py
@@ -0,0 +1,10 @@
+print("Welcome to Python!!")
+print(2 + 3)
+print(3 * 4)
+print(5 / 2)
+print(5 // 2)
+print(5 % 2)
+print(2 ** 5)
+print(7 % (5 // 2))
+
+print(0.01 * (2 ** 30)) #double the 0.01 number for 30 times
